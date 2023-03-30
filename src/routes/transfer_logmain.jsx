751787818d38562e6fe3a24555d1b71f1b237884
OkSangSoo <toy_369@kookmin.ac.kr>
2023-03-31 00:28:00 +0900

    거래내역화면 프로토타입

--- src/routes/transfer_logmain.jsx
+++ src/routes/transfer_logmain.jsx
@@ -1,23 +1,33 @@
 import { Link } from "react-router-dom";
 import { useState } from 'react';
-import { DownOutlined } from '@ant-design/icons';
-import { Dropdown } from 'antd';
+import { Select, Card } from 'antd';
 
 
 import "./bgImage.css";
 import "./transfer_logmain.css";
 
 
-
 function TransferLogMain() {
     const [selectedTerm, setSelectedTerm] = useState('지난 1개월간');
+    const [searchVal, setSearchVal] = useState("");
 
-    const items = [
-        {key:'1', label: '지난 1개월간',}, {key:'2', label: '지난 3개월간',}, {key:'3', label: '지난 6개월간',}, 
+    const transfers = [
+        {code:"DEP", amount: "100", account:"1234543", name:"AAA", date:"20230328", transferID:"1"},
+        {code:"WITH", amount: "200", account:"5432112", name:"BBB", date:"20230329", transferID:"2"},
+        {code:"DEP", amount: "150", account:"1234567", name:"CCC", date:"20230330", transferID:"3"},
     ];
 
-    const handleNext = e =>{
-        
+    const terms = [
+        {id:'1', value: '지난 1개월간',}, {id:'2', value: '지난 3개월간',}, {id:'3', value: '지난 6개월간',}, 
+    ];
+
+    const handleSearch = e =>{
+        console.log(selectedTerm+ " " +searchVal)
+    };
+
+    const handleSelect = e =>{
+        setSelectedTerm(e);
+        console.log(e);
     };
 
     return(
@@ -30,9 +40,22 @@ function TransferLogMain() {
                 </div>
                     
                 <div>
+                    <div className="filterGrid">
+                        <Select options = {terms} size = 'small' dropdownStyle={{textAlign : 'center' }}onChange={handleSelect} value={selectedTerm} className="selectTerm"></Select>
+                        <input onChange={e => setSearchVal(e.target.value)} placeholder="거래대상 입력" value={searchVal} className="searchBox"></input>
+                        <button onClick={handleSearch} className="searchButton">&#128270;</button>
+                    </div>
 
+                    <Card title="거래내역" className="mainBox">
+                        {transfers.map((transfer) => (
+                            <Card title={"Account : " + transfer.account} extra={transfer.code} key={transfer.transferID} className="detailBox">
+                                <p>{"Name : " + transfer.name}</p>
+                                <p>{"Amount : " + transfer.amount}</p>
+                                <p>{"Date : " + transfer.date}</p>
+                            </Card>
+                        ))}
+                    </Card>
                 </div>
-
             </div>
 
         </main>
